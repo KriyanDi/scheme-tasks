121ee3876057c32b45238415592ed799f4a30dbd
KriyanDi <kriyandimitrov@gmail.com>
2019-10-05 19:22:46 +0300

    Last tasks added

--- Week 2/7-count-devisors-rec.scm
+++ Week 2/7-count-devisors-rec.scm
@@ -1,2 +1,15 @@
-(define (count-devisors-rec n)
-    (if ())
+(define (count-devisor-rec n)
+    (count-devisor-rec-help 1 n))
+
+(define (count-devisor-rec-help counter number)
+    (cond 
+        ((= number 0) 
+            0)
+        ((> counter number)
+            0)
+        ((= (remainder number counter) 0)
+            (+ 1
+               (count-devisor-rec-help (+ counter 1)
+                                        number)))
+        (else (count-devisor-rec-help (+ counter 1)
+                                      number))))
---
+++ Week 2/8-sum-devisors-rec.scm
@@ -0,0 +1,15 @@
+(define (sum-devisor-rec n)
+    (sum-devisor-rec-help 1 n))
+
+(define (sum-devisor-rec-help counter number)
+    (cond 
+        ((= number 0) 
+            0)
+        ((> counter number)
+            0)
+        ((= (remainder number counter) 0)
+            (+ counter
+               (sum-devisor-rec-help (+ counter 1)
+                                        number)))
+        (else (sum-devisor-rec-help (+ counter 1)
+                                      number))))
