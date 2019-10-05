7795f6f384bd03768faf99980e9a59e0bdbd7a1e
KriyanDi <kriyandimitrov@gmail.com>
2019-10-05 18:05:04 +0300

    Added few more tasks

---
+++ Week 2/7-count-devisors-iter.scm
@@ -0,0 +1,13 @@
+(define (count-devisor-iter n)
+    (count-devisor-iter-help 0 1 n))
+
+(define (count-devisor-iter-help devisors counter max-count)
+    (if (<= counter max-count)
+        (if (= (remainder max-count counter) 0)
+            (count-devisor-iter-help (+ devisors 1)
+                                    (+ counter 1)
+                                    max-count)
+            (count-devisor-iter-help devisors
+                                    (+ counter 1)
+                                    max-count))
+        devisors))
---
+++ Week 2/8-sum-devisors-iter.scm
@@ -0,0 +1,13 @@
+(define (sum-devisor-iter n)
+    (sum-devisor-iter-help 0 1 n))
+
+(define (sum-devisor-iter-help sum counter max-count)
+    (if (<= counter max-count)
+        (if (= (remainder max-count counter) 0)
+            (sum-devisor-iter-help (+ sum counter)
+                                    (+ counter 1)
+                                    max-count)
+            (sum-devisor-iter-help sum
+                                    (+ counter 1)
+                                    max-count))
+        sum))
