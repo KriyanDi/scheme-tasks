4c684df98e2b6c20ae3a8710d8808502ed62534f
KriyanDi <kriyandimitrov@gmail.com>
2019-10-11 15:06:50 +0300

    added 3task

--- Week 3/3-binomial-coefficient-rec.scm
+++ Week 3/3-binomial-coefficient-rec.scm
@@ -1,8 +1,8 @@
-(define (binomial-coefficient row index)
+(define (binomial-coefficient-rec row index)
     (cond 
         ((or (= index 1)
              (= index row))
             1)
         (else
-            (+ (binomial-coefficient (- row 1) (- index 1))
-               (binomial-coefficient (- row 1) index)))))
+            (+ (binomial-coefficient-rec (- row 1) (- index 1))
+               (binomial-coefficient-rec (- row 1) index)))))
