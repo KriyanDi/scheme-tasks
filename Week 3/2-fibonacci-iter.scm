a6472727573575d3294a97399410454d0dcf22cd
KriyanDi <kriyandimitrov@gmail.com>
2019-10-11 11:28:29 +0300

    Added task 5

--- Week 3/2-fibonacci-iter.scm
+++ Week 3/2-fibonacci-iter.scm
@@ -2,14 +2,9 @@
     (fibonacci-iter-help 1 n 1 0))
 
 (define (fibonacci-iter-help counter count-max fib fib-1)
-
-    (cond 
-        ((= count-max 0) 
-            0)
-        ((= counter count-max) 
-            fib)
-        (else
-            (fibonacci-iter-help (+ counter 1) 
-                                 count-max 
-                                 (+ fib fib-1) 
-                                 fib))))
+    (cond ((= count-max 0)  0)
+          ((= counter count-max) fib)
+          (else (fibonacci-iter-help (+ counter 1) 
+                                     count-max 
+                                     (+ fib fib-1) 
+                                     fib))))
--- Week 3/5-f-iter.scm
+++ Week 3/5-f-iter.scm
@@ -1,2 +1,13 @@
 (define (f-iter n)
-    (f-iter-help ))
+    (f-iter-help 2 n 2 1 0))
+
+(define (f-iter-help counter count-max f f-1 f-2)
+    (cond ((< count-max 3)  count-max)
+          ((= counter count-max) f)
+          (else (f-iter-help (+ counter 1) 
+                             count-max 
+                             (+ f 
+                                (* 2 f-1)
+                                (* 3 f-2))
+                             f
+                             f-1))))
