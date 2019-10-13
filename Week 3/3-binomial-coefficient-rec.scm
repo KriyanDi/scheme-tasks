cbd5aeaed5c979f7bc392c855837ee77d4748d53
KriyanDi <kriyandimitrov@gmail.com>
2019-10-14 00:55:02 +0300

    Changed minor thing

--- Week 3/3-binomial-coefficient-rec.scm
+++ Week 3/3-binomial-coefficient-rec.scm
@@ -2,7 +2,7 @@
     (cond 
         ((or (= index 1)
              (= index row))
-            1)
+             1)
         (else
             (+ (binomial-coefficient-rec (- row 1) (- index 1))
                (binomial-coefficient-rec (- row 1) index)))))
