fe32857b621ce7372af23c77caf024c3fb54398a
KriyanDi <kriyandimitrov@gmail.com>
2019-10-19 20:37:00 +0300

    Added first part of 4week-tasks

--- Week 4/1-part/4-last.scm
+++ Week 4/1-part/4-last.scm
@@ -1,20 +1,25 @@
-(define (accumulate combine null-value term list)
+;; (define (accumulate combine null-value term list)
+;;     (if (null? list)
+;;         null-value
+;;         (combine (term list)
+;;                  (accumulate combine
+;;                              null-value
+;;                              term
+;;                              (cdr list)))))
+
+;; (define (fold null-value combine term list)
+;;     (accumulate combine null-value term list))
+
+;; (define (lenght list)
+;;     (fold 0 
+;;           + 
+;;           (lambda (list) 1)
+;;           list))
+
+(define (length list)
     (if (null? list)
-        null-value
-        (combine (term list)
-                 (accumulate combine
-                             null-value
-                             term
-                             (cdr list)))))
-
-(define (fold null-value combine term list)
-    (accumulate combine null-value term list))
-
-(define (lenght list)
-    (fold 0 
-          + 
-          (lambda (list) 1)
-          list))
+        0
+        (+ 1 (length (cdr list)))))
 
 (define (last list)
     (if (<= (lenght list) 1)
